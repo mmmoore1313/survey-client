0ef3f4ebde2be0f48089fa0be6f18774493c574a
Matt Moore <Matt.M.Moore13@outlook.com>
2021-04-10 19:18:47 -0400

    Initially was using this branch to reconfigure the app for React Hooks (for greater ease with styling functionality) and did not create a new branch for the CheeseWheel changes. That said, this commit contains much of the rearrangement for both the CheeseWheel functionality and the beginnings of Hook transformation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component, Fragment } from 'react'
-import { Route } from 'react-router-dom'
-import { v4 as uuid } from 'uuid'
-// this is a comment
+import React, { Fragment, useState } from 'react'
+import { Route, withRouter } from 'react-router-dom'
+
+// styles
+import 'bootstrap/scss/bootstrap.scss'
+
 import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
 import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
 
@@ -10,101 +12,79 @@ import SignIn from './components/SignIn/SignIn'
 import SignOut from './components/SignOut/SignOut'
 import ChangePassword from './components/ChangePassword/ChangePassword'
 import TakeAsurvey from './components/TakeAsurvey/TakeAsurvey'
-// import crud components
 import SurveyResponse from './components/SurveyResponse/SurveyResponse'
-// import crud components
-import SurveyCreate from './components/routes/SurveyCreate'
-import SurveyShow from './components/routes/SurveyShow'
-import SurveyUpdate from './components/routes/SurveyUpdate'
-import SurveyIndex from './components/routes/SurveyIndex'
-// import layout
-import Header from './components/shared/Header'
-import Home from './components/shared/Home'
-import Footer from './components/shared/Footer'
+import { SurveyCreate, SurveyShow, SurveyUpdate, SurveyIndex } from './components/routes'
+import CwCreate from './components/routes/Cheesewheel/cwCreate'
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      user: null,
-      msgAlerts: []
+function App (props) {
+  const [ user ] = useState(null)
+  const [ msgAlerts ] = useState([])
+
+  const setUser = user
+  const clearUser = user.useState(null)
+  const deleteAlert (id) =
+    useState((state) => {
+      return { msgAlerts: msgAlerts.filter(msg => msg.id !== id) }
+      }
     }
-  }
+  )
 
-  setUser = user => this.setState({ user })
-
-  clearUser = () => this.setState({ user: null })
-
-  deleteAlert = (id) => {
-    this.setState((state) => {
-      return { msgAlerts: state.msgAlerts.filter(msg => msg.id !== id) }
-    })
-  }
-
-  msgAlert = ({ heading, message, variant }) => {
-    const id = uuid()
-    this.setState((state) => {
-      return { msgAlerts: [...state.msgAlerts, { heading, message, variant, id }] }
-    })
-  }
-
-  render (props) {
-    const { msgAlerts, user } = this.state
-
-    return (
-      <Fragment>
-
-        {msgAlerts.map(msgAlert => (
-          <AutoDismissAlert
-            key={msgAlert.id}
-            heading={msgAlert.heading}
-            variant={msgAlert.variant}
-            message={msgAlert.message}
-            id={msgAlert.id}
-            deleteAlert={this.deleteAlert}
-          />
-        ))}
-
-        <Header user={user} />
-        <Home user={user} />
-        <Footer />
-        <Route path='/sign-up' render={() => (
-          <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
-        )} />
-        <Route path='/sign-in' render={() => (
-          <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
-        )} />
-        <Route path='/take-survey' render={() => (
-          <TakeAsurvey msgAlert={this.msgAlert}/>
-        )} />
-        <Route exact path='/response/:surveyID/:participantID' render={() => (
-          <SurveyResponse msgAlert={this.msgAlert}/>
-        )} />
-        <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-          <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
-        )} />
-        <AuthenticatedRoute user={user} path='/change-password' render={() => (
-          <ChangePassword msgAlert={this.msgAlert} user={user} />
-        )} />
-        {/* create a survey */}
-        <AuthenticatedRoute user={user} path='/create-survey' render={() => (
-          <SurveyCreate msgAlert={this.msgAlert} user={user} />
-        )} />
-        {/* Update the survey */}
-        <AuthenticatedRoute user={user} exact path='/surveys/:id/edit' render={() => (
-          <SurveyUpdate msgAlert={this.msgAlert} user={user} />
-        )} />
-        {/* Show the survey */}
-        <AuthenticatedRoute user={user} exact path='/surveys/:id' render={() => (
-          <SurveyShow msgAlert={this.msgAlert} user={user} />
-        )} />
-        {/* Show ALL surveys */}
-        <AuthenticatedRoute user={user} exact path='/surveys' render={() => (
-          <SurveyIndex msgAlert={this.msgAlert} user={user} />
-        )} />
+  return (
+    <React.Fragment>
+      <div className="App">
+        {props.location.state ? props.location.state.msg : null}
+      </div>
+      <Fragment id='routes'>
+        <Fragment id='unauthenticated'>
+          <Fragment id='auth'>
+            <Route path='/sign-up' render={() => (
+              <SignUp msgAlert={this.msgAlert} setUser={setUser} />
+            )} />
+            <Route path='/sign-in' render={() => (
+              <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+            )} />
+          </Fragment>
+          <Fragment id='participant'>
+            <Route path='/take-survey' render={() => (
+              <TakeAsurvey msgAlert={this.msgAlert}/>
+            )} />
+            <Route exact path='/response/:surveyID/:participantID' render={() => (
+              <SurveyResponse msgAlert={this.msgAlert}/>
+            )} />
+          </Fragment>
+        </Fragment>
+        <Fragment id='authenticated'>
+          <Fragment id='userRoutes'>
+            <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+              <SignOut msgAlert={this.msgAlert} clearUser={clearUser} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/change-password' render={() => (
+              <ChangePassword msgAlert={this.msgAlert} user={user} />
+            )} />
+          </Fragment>
+        </Fragment>
+        <Fragment id='cheesewheel'>
+          <AuthenticatedRoute user={user} path='/create-cheesewheel' render={() => (
+            <CwCreate msgAlert={this.msgAlert} user={user} />
+          )} />
+        </Fragment>
+        <Fragment id='survey'>
+          <AuthenticatedRoute user={user} path='/create-survey' render={() => (
+            <SurveyCreate msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/surveys/:id/edit' render={() => (
+            <SurveyUpdate msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/surveys/:id' render={() => (
+            <SurveyShow msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/surveys' render={() => (
+            <SurveyIndex msgAlert={this.msgAlert} user={user} />
+          )} />
+        </Fragment>
       </Fragment>
-    )
-  }
+    </React.Fragment>
+  )
 }
 
-export default App
+export default withRouter(App)
